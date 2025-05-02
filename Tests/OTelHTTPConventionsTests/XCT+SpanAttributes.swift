58f01a12b9ed9f096dd6fc7b588c1bb7ed4896f5
James Muldoon <jmuldoon@apple.com>
2025-05-02 12:01:46 +0900

    Add Kubernetes semantics to SpanAttributes
    Built out a suite of Kubernetes SpanAttributes conforming to SemConv of Otel. This adheres/expands on <https://github.com/open-telemetry/semantic-conventions/blob/v1.32.0/docs/resource/k8s.md>

---
+++ Tests/OTelHTTPConventionsTests/XCT+SpanAttributes.swift
@@ -0,0 +1,33 @@
+import Tracing
+import XCTest
+
+func XCTAssertSpanAttributesEqual(
+    _ lhs: @autoclosure () -> SpanAttributes,
+    _ rhs: @autoclosure () -> [String: SpanAttribute],
+    file: StaticString = #file,
+    line: UInt = #line
+) {
+    var rhs = rhs()
+
+    lhs().forEach { key, attribute in
+        if let rhsValue = rhs.removeValue(forKey: key) {
+            if rhsValue != attribute {
+                XCTFail(
+                    #""\#(key)" was expected to be "\#(rhsValue)" but is actually "\#(attribute)"."#,
+                    file: file,
+                    line: line
+                )
+            }
+        } else {
+            XCTFail(
+                #"Did not specify expected value for "\#(key)", actual value is "\#(attribute)"."#,
+                file: file,
+                line: line
+            )
+        }
+    }
+
+    if !rhs.isEmpty {
+        XCTFail(#"Expected attributes "\#(rhs.keys)" are not present in actual attributes."#, file: file, line: line)
+    }
+}
