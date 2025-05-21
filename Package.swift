aa62725410f1dff057599a62428d65d7c16a03dd
Jay Herron <NeedleInAJayStack@protonmail.com>
2025-05-20 22:09:17 -0600

    feat: Removes original Metrics libraries
    This removes the `OTelSemanticMetricsConventions`, `OTelHTTPConventions`, and `OTelHTTPMetricsConventions` libraries, along with source code and tests

--- Package.swift
+++ Package.swift
@@ -4,40 +4,13 @@ import PackageDescription
 let package = Package(
     name: "swift-otel-semantic-conventions",
     products: [
-        .library(name: "OTelSemanticMetricsConventions", targets: ["OTelHTTPMetricsConventions"]),
-        .library(
-            name: "OTelHTTPConventions",
-            targets: ["OTelHTTPMetricsConventions"]
-        ),
-        .library(name: "OTelHTTPMetricsConventions", targets: ["OTelHTTPMetricsConventions"]),
         .library(name: "OTelSemanticConventions", targets: ["OTelSemanticConventions"]),
         .library(name: "OTelSpanConventions", targets: ["OTelSpanConventions"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-metrics.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-distributed-tracing.git", from: "1.0.0"),
     ],
     targets: [
-        .target(
-            name: "OTelHTTPMetricsConventions",
-            dependencies: [
-                .product(name: "CoreMetrics", package: "swift-metrics"),
-                .target(name: "OTelMetricsConventionsCore"),
-            ]
-        ),
-        .testTarget(
-            name: "OTelHTTPConventionsTests",
-            dependencies: [
-                .product(name: "Metrics", package: "swift-metrics"),
-                .product(name: "Tracing", package: "swift-distributed-tracing"),
-                .target(name: "OTelHTTPMetricsConventions"),
-                .target(name: "OTelMetricsConventionsTestSupport"),
-            ]
-        ),
-
-        .target(name: "OTelMetricsConventionsCore"),
-        .target(name: "OTelMetricsConventionsTestSupport"),
-
         .target(name: "OTelSemanticConventions"),
         .target(
             name: "OTelSpanConventions",
