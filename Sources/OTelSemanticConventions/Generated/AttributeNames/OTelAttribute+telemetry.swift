72214de1286b4671c7505b2fa7a14f77d0d8bc3a
Jay Herron <30518755+NeedleInAJayStack@users.noreply.github.com>
2025-05-23 01:41:06 -0600

    Generated files for all non-http namespaces (#9)
    * feat: Adds all other namespaces
    
    * feat: Excludes non-swift runtime attributes by default
    
    This includes the "aspnetcore", "jvm", "nodejs", "signalr", and "v8js" namespaces.

---
+++ Sources/OTelSemanticConventions/Generated/AttributeNames/OTelAttribute+telemetry.swift
@@ -0,0 +1,48 @@
+// DO NOT EDIT. This file is generated automatically. See README for details.
+
+extension OTelAttribute {
+    /// `telemetry` namespace
+    public enum telemetry {
+        /// `telemetry.sdk` namespace
+        public enum sdk {
+            /// `telemetry.sdk.language`: The language of the telemetry SDK.
+            ///
+            /// - Stability: stable
+            ///
+            /// - Type: enum
+            ///     - `cpp`
+            ///     - `dotnet`
+            ///     - `erlang`
+            ///     - `go`
+            ///     - `java`
+            ///     - `nodejs`
+            ///     - `php`
+            ///     - `python`
+            ///     - `ruby`
+            ///     - `rust`
+            ///     - `swift`
+            ///     - `webjs`
+            public static let language = "telemetry.sdk.language"
+
+            /// `telemetry.sdk.name`: The name of the telemetry SDK as defined above.
+            ///
+            /// - Stability: stable
+            ///
+            /// - Type: string
+            ///
+            /// The OpenTelemetry SDK MUST set the `telemetry.sdk.name` attribute to `opentelemetry`. If another SDK, like a fork or a vendor-provided implementation, is used, this SDK MUST set the `telemetry.sdk.name` attribute to the fully-qualified class or module name of this SDK's main entry point or another suitable identifier depending on the language. The identifier `opentelemetry` is reserved and MUST NOT be used in this case. All custom identifiers SHOULD be stable across different versions of an implementation.
+            ///
+            /// - Example: `opentelemetry`
+            public static let name = "telemetry.sdk.name"
+
+            /// `telemetry.sdk.version`: The version string of the telemetry SDK.
+            ///
+            /// - Stability: stable
+            ///
+            /// - Type: string
+            ///
+            /// - Example: `1.2.3`
+            public static let version = "telemetry.sdk.version"
+        }
+    }
+}
---
+++ Sources/OTelSemanticConventions/Generated/Tracing/SpanAttributes+telemetry.swift
@@ -0,0 +1,125 @@
+// DO NOT EDIT. This file is generated automatically. See README for details.
+
+#if Tracing
+
+import Tracing
+
+extension SpanAttributes {
+    /// `telemetry` namespace
+    public var telemetry: TelemetryAttributes {
+        get {
+            .init(attributes: self)
+        }
+        set {
+            self = newValue.attributes
+        }
+    }
+
+    @dynamicMemberLookup
+    public struct TelemetryAttributes: SpanAttributeNamespace {
+        public var attributes: SpanAttributes
+
+        public init(attributes: SpanAttributes) {
+            self.attributes = attributes
+        }
+
+        public struct NestedSpanAttributes: NestedSpanAttributesProtocol {
+            public init() {}
+        }
+
+        /// `telemetry.sdk` namespace
+        public var sdk: SdkAttributes {
+            get {
+                .init(attributes: self.attributes)
+            }
+            set {
+                self.attributes = newValue.attributes
+            }
+        }
+
+        @dynamicMemberLookup
+        public struct SdkAttributes: SpanAttributeNamespace {
+            public var attributes: SpanAttributes
+
+            public init(attributes: SpanAttributes) {
+                self.attributes = attributes
+            }
+
+            public struct NestedSpanAttributes: NestedSpanAttributesProtocol {
+                public init() {}
+
+                /// `telemetry.sdk.language`: The language of the telemetry SDK.
+                ///
+                /// - Stability: stable
+                ///
+                /// - Type: enum
+                ///     - `cpp`
+                ///     - `dotnet`
+                ///     - `erlang`
+                ///     - `go`
+                ///     - `java`
+                ///     - `nodejs`
+                ///     - `php`
+                ///     - `python`
+                ///     - `ruby`
+                ///     - `rust`
+                ///     - `swift`
+                ///     - `webjs`
+                public var language: Self.Key<LanguageEnum> { .init(name: OTelAttribute.telemetry.sdk.language) }
+
+                public struct LanguageEnum: SpanAttributeConvertible, Sendable {
+                    private let rawValue: String
+                    /// `cpp`
+                    public static let cpp = Self.init(rawValue: "cpp")
+                    /// `dotnet`
+                    public static let dotnet = Self.init(rawValue: "dotnet")
+                    /// `erlang`
+                    public static let erlang = Self.init(rawValue: "erlang")
+                    /// `go`
+                    public static let go = Self.init(rawValue: "go")
+                    /// `java`
+                    public static let java = Self.init(rawValue: "java")
+                    /// `nodejs`
+                    public static let nodejs = Self.init(rawValue: "nodejs")
+                    /// `php`
+                    public static let php = Self.init(rawValue: "php")
+                    /// `python`
+                    public static let python = Self.init(rawValue: "python")
+                    /// `ruby`
+                    public static let ruby = Self.init(rawValue: "ruby")
+                    /// `rust`
+                    public static let rust = Self.init(rawValue: "rust")
+                    /// `swift`
+                    public static let swift = Self.init(rawValue: "swift")
+                    /// `webjs`
+                    public static let webjs = Self.init(rawValue: "webjs")
+                    public func toSpanAttribute() -> Tracing.SpanAttribute {
+                        .string(self.rawValue)
+                    }
+                }
+
+                /// `telemetry.sdk.name`: The name of the telemetry SDK as defined above.
+                ///
+                /// - Stability: stable
+                ///
+                /// - Type: string
+                ///
+                /// The OpenTelemetry SDK MUST set the `telemetry.sdk.name` attribute to `opentelemetry`. If another SDK, like a fork or a vendor-provided implementation, is used, this SDK MUST set the `telemetry.sdk.name` attribute to the fully-qualified class or module name of this SDK's main entry point or another suitable identifier depending on the language. The identifier `opentelemetry` is reserved and MUST NOT be used in this case. All custom identifiers SHOULD be stable across different versions of an implementation.
+                ///
+                /// - Example: `opentelemetry`
+                public var name: Self.Key<String> { .init(name: OTelAttribute.telemetry.sdk.name) }
+
+                /// `telemetry.sdk.version`: The version string of the telemetry SDK.
+                ///
+                /// - Stability: stable
+                ///
+                /// - Type: string
+                ///
+                /// - Example: `1.2.3`
+                public var version: Self.Key<String> { .init(name: OTelAttribute.telemetry.sdk.version) }
+            }
+        }
+    }
+}
+
+#endif
