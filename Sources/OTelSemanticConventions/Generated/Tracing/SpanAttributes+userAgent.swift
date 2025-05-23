af02a304c2f9fc14c7f478318f665ec0e6612067
Jay Herron <NeedleInAJayStack@protonmail.com>
2025-05-22 20:33:20 -0600

    feat: Adds all other namespaces

---
+++ Sources/OTelSemanticConventions/Generated/Tracing/SpanAttributes+userAgent.swift
@@ -0,0 +1,44 @@
+// DO NOT EDIT. This file is generated automatically. See README for details.
+
+#if Tracing
+
+import Tracing
+
+extension SpanAttributes {
+    /// `user_agent` namespace
+    public var userAgent: UserAgentAttributes {
+        get {
+            .init(attributes: self)
+        }
+        set {
+            self = newValue.attributes
+        }
+    }
+
+    @dynamicMemberLookup
+    public struct UserAgentAttributes: SpanAttributeNamespace {
+        public var attributes: SpanAttributes
+
+        public init(attributes: SpanAttributes) {
+            self.attributes = attributes
+        }
+
+        public struct NestedSpanAttributes: NestedSpanAttributesProtocol {
+            public init() {}
+
+            /// `user_agent.original`: Value of the [HTTP User-Agent](https://www.rfc-editor.org/rfc/rfc9110.html#field.user-agent) header sent by the client.
+            ///
+            /// - Stability: stable
+            ///
+            /// - Type: string
+            ///
+            /// - Examples:
+            ///     - `CERN-LineMode/2.15 libwww/2.17b3`
+            ///     - `Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Mobile/15E148 Safari/604.1`
+            ///     - `YourApp/1.0.0 grpc-java-okhttp/1.27.2`
+            public var original: Self.Key<String> { .init(name: OTelAttribute.userAgent.original) }
+        }
+    }
+}
+
+#endif
